bed16b5de7b519cdc27d59dc60a4db9a46180ffa
Fábio Roberto Teodoro <fr.teodoro@gmail.com>
2017-05-21 18:19:24 -0300

    Added an empty TaskList component.

--- static/frontend/src/app/app.module.ts
+++ static/frontend/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { TaskListComponent } from './task_list/task_list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskListComponent
   ],
   imports: [
     BrowserModule,
---
+++ static/frontend/src/app/task_list/task_list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-task_list',
+    templateUrl: './task_list.component.html'
+})
+export class TaskListComponent {
+}
