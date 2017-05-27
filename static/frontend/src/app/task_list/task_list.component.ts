818600d698c46f5dbe83117f43fb81c7555f596d
Fábio Roberto Teodoro <fr.teodoro@gmail.com>
2017-05-27 12:24:58 -0300

    Getting the task list from the backend via HTTP.

--- static/frontend/src/app/task_list/task_list.component.ts
+++ static/frontend/src/app/task_list/task_list.component.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
     selector: 'app-task_list',
-    templateUrl: './task_list.component.html'
+    templateUrl: './task_list.component.html',
+    styleUrls: ['./task_list.component.css']
 })
 export class TaskListComponent {
-    tasks = ['happy Task', 'sad Task'];
+    tasks = [];
+
+    constructor(private http : Http) {}
+
+    ngOnInit() {
+        this.fetchTasks();
+    }
+    
+    fetchTasks() {
+        this.http.get('http://localhost:3000/tasks').subscribe(
+            (data) => {
+                this.tasks = JSON.parse(data['_body']).tasks;
+            }
+        );
+    }
 }
