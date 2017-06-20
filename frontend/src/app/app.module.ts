18c1bb8d75b41dd8c56d5f31441699fb4f86a3f7
Fábio Roberto Teodoro <fr.teodoro@gmail.com>
2017-06-19 22:19:12 -0300

    Added routing to the frontent.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,21 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TaskListComponent } from './task_list/task_list.component';
+import { NewTaskComponent } from './new-task/new-task.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TaskListComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        TaskListComponent,
+        NewTaskComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        RouterModule.forRoot([
+            {
+                path: 'task_list',
+                component: TaskListComponent
+            },
+            {
+                path: 'new_task',
+                component: NewTaskComponent
+            },
+            {
+                path: '',
+                redirectTo: '/task_list',
+                pathMatch: 'full'
+            }
+ 
+        ])
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
