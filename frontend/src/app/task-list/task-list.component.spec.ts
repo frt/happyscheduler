f0ecc21d81cec0d0d5b0c3fd2602060f020d15a5
Fábio Roberto Teodoro <fr.teodoro@gmail.com>
2017-07-08 18:42:12 -0300

    Just some refactoring.

--- frontend/src/app/task-list/task-list.component.spec.ts
+++ frontend/src/app/task-list/task-list.component.spec.ts
@@ -6,6 +6,8 @@ import { InfoMessagesService } from '../info-messages.service';
 import { TaskListComponent } from './task-list.component';
 
 describe('TaskListComponent', () => {
+    let fixture;
+    let nodes;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -17,20 +19,18 @@ describe('TaskListComponent', () => {
                 HttpModule
             ]
         }).compileComponents();
+        fixture = TestBed.createComponent(TaskListComponent);
+        nodes = fixture.debugElement.nativeElement;
     }));
 
     it('should not have an <hr> element',  async(() => {
-        const fixture = TestBed.createComponent(TaskListComponent);
-        const nodes = fixture.debugElement.nativeElement;
         expect(nodes.querySelectorAll('hr').length).toEqual(0);
     }));
 
-    it('should have a delete button for each task', async(() => {
-        const fixture = TestBed.createComponent(TaskListComponent);
-        const nodes = fixture.debugElement.nativeElement;
-
+    it('should have a delete button', async(() => {
         fixture.componentInstance.tasks = [{name: 'test remove', happy: true}];
         fixture.detectChanges();
+
         expect(nodes.querySelectorAll('button > .glyphicon-remove').length)
             .toBeGreaterThan(0);
     }));
